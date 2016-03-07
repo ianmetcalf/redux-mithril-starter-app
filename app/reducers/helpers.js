e097405d1d813664564d8cc8420a363271cb6a14
Ian Metcalf <ian@metcalfbuilt.com>
2016-03-07 10:56:38 -0500

    Added error checking and tests for createReducer()

--- app/reducers/helpers.js
+++ app/reducers/helpers.js
@@ -1,5 +1,19 @@
-export function createReducer(initialState, handlers) {
-  return (state = initialState, action) => {
+const INVALID_KEYS = ['', 'undefined', 'null'];
+
+export function createReducer(initialState, handlers = {}) {
+  let count = 0;
+
+  for (const key in handlers) {
+    if (handlers.hasOwnProperty(key) && INVALID_KEYS.indexOf(key) < 0) {
+      count += 1;
+    } else {
+      throw new Error('Unexpected action type found while creating reducer');
+    }
+  }
+
+  if (!count) throw new Error('No action types found while creating reducer');
+
+  return function reducer(state = initialState, action) {
     const handler = handlers[action.type] || handlers[createReducer.DEFAULT];
 
     if (handler) {
